544e34e844f92b2c59c113ace9bda7ffc9da91fe
Ubuntu <krojas1171@gmail.com>
2016-06-13 14:30:18 -0500

    added method for address to use it on geocoder gem

--- app/models/titulation.rb
+++ app/models/titulation.rb
@@ -6,4 +6,13 @@ class Titulation < ActiveRecord::Base
 	attr_accessor :tvia, :via, :adicional, :num, :compvia, :placa, :compnum
 
 	before_save :address_hash
+	after_save :full_street_address
+
+	geocoded_by :full_street_address   # can also be an IP address
+	after_validation :geocode,       # auto-fetch coordinates
+
+	def full_street_address
+		a = claimant_address.values.join('')
+		a + ', ' + municipio + ',' + depart
+	end
 end
